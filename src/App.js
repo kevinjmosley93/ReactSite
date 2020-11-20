7c8eecc396b894b47e2ee1a24385b85f51e3c008
kevinjmosley93 <kevinjmosley93@gmail.com>
2020-11-19 18:52:40 -0600

    Latest Commit

--- src/App.js
+++ src/App.js
@@ -4,14 +4,11 @@ import About from './components/routes/About'
 import Contact from './components/routes/Contact'
 import Home from './components/routes/Home'
 import Nav from './components/shared/Nav'
-import families from './data/gameOfThrones'
 
 const App = () => (
   <Fragment>
     <Route path='/' component={Nav} />
-    <Route exact path='/' render={() => (
-      families.map(family => <Home key={family.id} name={family.name} suffix={family.wikiSuffix } people={family.people }/>)
-    )} />
+    <Route exact path='/' component={Home} />
     <Route exact path='/about' component={About} />
     <Route exact path='/contact' component={Contact} />
   </Fragment>
--- src/components/routes/Home.js
+++ src/components/routes/Home.js
@@ -1,15 +1,9 @@
 import React from 'react'
-import Person from '../Person'
 
-const Home = (props) => {
-    const { name, suffix, people} = props
-    // console.log('home name', people)
+const Home = () => {
     return (
         <div>
-            <h1> the {name} family aka {suffix} their names are below </h1>
-            {people.map(person => {
-                console.log('home person', person)
-                return (<Person key={ person.id } name={person.name} />)})}
+           <h1>This is Home</h1>
         </div>
     )
 }
